094696a53bd7abcc72972f681e8bed379a83b6b8
zimlit <zimlit2010@gmail.com>
2021-02-16 11:38:24 -0500

    coding component finished for now

--- src/coding.js
+++ src/coding.js
@@ -1,9 +1,27 @@
 import Header from "./header";
+import "./coding.css"
 
 function Coding() {
 	return (
 		<div class="coding">
 			<Header value={["home", "cubing", "conlanging", "worldbuilding"]}/>
+			<p class="github">
+				<a class="github-button" href="https:/github.com/zimlit">github</a>
+			</p>
+			<section class="cat">
+					<h1 class="label">
+                Projects 
+            </h1>
+            <h2 class="cat-button">
+                <a class="cat-button" href="https:/github.com/zimlit/zimlit.com-react">This website</a>
+            </h2>
+            <h2>
+								<a class="cat-button" href="https:/github.com/zimlit/valley">the valley programing language</a>
+            </h2>
+            <h2>
+
+            </h2>
+			</section>
 		</div>
 	);
 }
